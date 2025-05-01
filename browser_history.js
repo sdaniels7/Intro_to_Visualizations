e66e9c0e67dd165574d3f1d491d5153a2bcc3eb9
sdaniels7 <147332665+sdaniels7@users.noreply.github.com>
2025-04-30 21:53:59 -0400

    script.js

--- browser_history.js
+++ browser_history.js
@@ -1,14 +1,3 @@
-<javascript>
-  <script>
-    d3.csv("FinalAggregatedDataset.csv")
-      .then(function (data) {
-        console.log(data); // See the loaded data
-      .catch(function (error) {
-        console.error("Error loading the CSV file:", error);
-      });
-
-</javascript>
-
 <script>
   d3.csv("FinalAggregatedDataset.csv"), d3.autoType)
     .then(function (data) {
