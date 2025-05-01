6020665cfcccc3e9d1e2ddd60c165ac1ee02e51f
sdaniels7 <147332665+sdaniels7@users.noreply.github.com>
2025-04-30 21:52:11 -0400

    Update browser_history.js

--- browser_history.js
+++ browser_history.js
@@ -8,3 +8,13 @@
       });
 
 </javascript>
+
+<script>
+  d3.csv("FinalAggregatedDataset.csv"), d3.autoType)
+    .then(function (data) {
+      console.log("✅ CSV loaded:", data); // Should log an array of objects
+    })
+    .catch(function (error) {
+      console.error("❌ Error loading CSV:", error);
+    });
+</script>
